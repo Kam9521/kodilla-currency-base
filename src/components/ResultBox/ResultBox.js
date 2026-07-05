782cac2a5a62d7e4e4dc90ef6faaa676650e9fb6
Kamil <szpmytyl@wp.pl>
2026-07-05 18:24:57 +0200

    Add tests for ResultBox component

--- src/components/ResultBox/ResultBox.js
+++ src/components/ResultBox/ResultBox.js
@@ -1,23 +1,29 @@
-import PropTypes from 'prop-types';
-import { convertUSDToPLN } from './../../utils/convertUSDToPLN';
-import { convertPLNToUSD } from './../../utils/convertPLNToUSD';
-import { formatAmountInCurrency } from './../../utils/formatAmountInCurrency';
-import { useMemo } from 'react';
-import styles from './ResultBox.module.scss';
+import PropTypes from "prop-types";
+import { convertUSDToPLN } from "./../../utils/convertUSDToPLN";
+import { convertPLNToUSD } from "./../../utils/convertPLNToUSD";
+import { formatAmountInCurrency } from "./../../utils/formatAmountInCurrency";
+import { useMemo } from "react";
+import styles from "./ResultBox.module.scss";
 
 const ResultBox = ({ from, to, amount }) => {
-
   const convertedAmount = useMemo(() => {
-    if(from === 'USD' && to === 'PLN') return convertUSDToPLN(amount);
-    if(from === 'PLN' && to === 'USD') return convertPLNToUSD(amount);
+    if (amount < 0) return "Wrong value...";
+    if (from === "USD" && to === "PLN") return convertUSDToPLN(amount);
+    if (from === "PLN" && to === "USD") return convertPLNToUSD(amount);
     return formatAmountInCurrency(amount, from);
   }, [from, to, amount]);
 
-  const formattedAmount = useMemo(() => formatAmountInCurrency(amount, from), [amount, from]);
+  const formattedAmount = useMemo(
+    () =>
+      amount < 0 ? "Wrong value..." : formatAmountInCurrency(amount, from),
+    [amount, from],
+  );
 
   return (
-    <div className={styles.result}>
-      {formattedAmount} = {convertedAmount}
+    <div className={styles.result} data-testid="output">
+      {amount < 0
+        ? "Wrong value..."
+        : `${formattedAmount} = ${convertedAmount}`}
     </div>
   );
 };
@@ -26,6 +32,6 @@ ResultBox.propTypes = {
   from: PropTypes.string.isRequired,
   to: PropTypes.string.isRequired,
   amount: PropTypes.number.isRequired,
-}
+};
 
 export default ResultBox;
